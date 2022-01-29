ef743b755b011608e58ca2068ddb473e94448f09
orvino <91889241+orvino@users.noreply.github.com>
2022-01-29 15:20:55 -0600

    update

--- config.py
+++ config.py
@@ -1,4 +1,1 @@
-weather_api_key="ec17b443f12e96353e0696ae7c9cf742"
-g_key = "AIzaSyAEokZm7O5tEN889bATZJ61CYInbPePhX4"
-nyt = 0dK7gcLjRqTRaoW3UIKB9z8PmDUSHtPJ
-omdb = 38371d62
+weather_api_key = "ec17b443f12e96353e0696ae7c9cf742"
